7039a9cc5c779c8b6c86789030c97b93773337c8
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-17 17:43:23 -0300

    Criando e logando usuario

---
+++ app/src/main/java/com/danieldonato/olxclone/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,35 @@
+package com.danieldonato.olxclone.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+    private static StorageReference referenciaStorage;
+    private static FirebaseAuth referenciaAuth;
+
+    public static DatabaseReference getFirebase() {
+        if(referenciaFirebase == null){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFirebase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao() {
+        if(referenciaAuth == null){
+            referenciaAuth = FirebaseAuth.getInstance();
+        }
+        return referenciaAuth;
+    }
+
+    public static StorageReference getFirebaseStorage(){
+        if(referenciaStorage == null){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenciaStorage;
+    }
+}
