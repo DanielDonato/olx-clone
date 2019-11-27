40164bbc7db3d0d89b03e4fc7814d5e6b05251db
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-26 22:48:10 -0300

    Mascaras

--- app/src/main/java/com/danieldonato/olxclone/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/danieldonato/olxclone/activity/CadastrarAnuncioActivity.java
@@ -6,17 +6,38 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.danieldonato.olxclone.R;
+import com.santalu.maskedittext.MaskEditText;
+
+import java.util.Currency;
+import java.util.Locale;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        inicializarComponentes();
         setSupportActionBar(toolbar);
     }
 
+
+
+    private void inicializarComponentes(){
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTitulo = findViewById(R.id.editTelefone);
+        Locale locale = new Locale("pt", "BR");
+        campoValor.setLocale(locale);
+    }
 }
